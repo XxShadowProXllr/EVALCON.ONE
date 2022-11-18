51ca3fd477cd1c1dba5ba8b84c5b001c39f1d285
XxShadowProXllr <xxshadowproxllr@gmail.com>
2022-11-18 16:35:50 -0500

    modified:   css/STYLES.css 	new file:   css/base 1.css 	modified:   css/base.css 	new file:   css/demo.css 	modified:   css/main.css 	new file:   images/RETIE.jpg 	renamed:    images/Logoevalcon.jpg -> images/RETILAP.jpg 	new file:   images/red-sky-g7e083185b_1920.jpg 	new file:   images/tower-gabfe87bdf_1920.jpg 	modified:   inc/sendEmail.php 	new file:   index-particles.html 	new file:   index-slides.html 	new file:   index-static.html 	modified:   index.html 	modified:   js/terminos.js

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -4,66 +4,20 @@
 $siteOwnersEmail = 'user@website.com';
 
 
-if($_POST) {
-
-    $name = trim(stripslashes($_POST['contactName']));
-    $email = trim(stripslashes($_POST['contactEmail']));
-    $subject = trim(stripslashes($_POST['contactSubject']));
-    $contact_message = trim(stripslashes($_POST['contactMessage']));
-
-    // Check Name
-    if (strlen($name) < 2) {
-        $error['name'] = "Please enter your name.";
+if (isset($_POST['enviar'])) {
+// Create the Variable's
+    if(!empty($_POST['contactName']) && !empty($_POST['contactEmail']) && !empty($_POST ['contactSubject'] && !empty($_POST['contactMessage']))) {
+        $name = $_POST['contactName'];
+        $email = $_POST['contactEmail'];
+        $subject =  $_POST['contactSubject'];
+        $contact_message = $_POST['contactMessage'];
+        $header = "From: noreply@example.com" . "\r\n";
+        $header. = "Reply-to: noreply@example.com" . "\r\n";
+        $header. = "X-Mailer: PHP/" . phpversion();
+        $mail = @mail($name,$email,$subject,$contact_message,$header);
+        if ($mail) {
+            echo "<h4>¡Mail Enviado Correctamente!</h4>";
+        }
     }
-    // Check Email
-    if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-        $error['email'] = "Please enter a valid email address.";
     }
-    // Check Message
-    if (strlen($contact_message) < 15) {
-        $error['message'] = "Please enter your message. It should have at least 15 characters.";
-    }
-    // Subject
-    if ($subject == '') { $subject = "Contact Form Submission"; }
-
-
-    // Set Message
-    $message .= "Email from: " . $name . "<br />";
-    $message .= "Email address: " . $email . "<br />";
-    $message .= "Message: <br />";
-    $message .= $contact_message;
-    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
-    // Set From: header
-    $from =  $name . " <" . $email . ">";
-
-    // Email Headers
-    $headers = "From: " . $from . "\r\n";
-    $headers .= "Reply-To: ". $email . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-
-    if (!$error) {
-
-        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-        if ($mail) { echo "OK"; }
-        else { echo "Something went wrong. Please try again."; }
-        
-    } # end if - no validation error
-
-    else {
-
-        $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-        $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-        $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-        
-        echo $response;
-
-    } # end if - there was a validation error
-
-}
-
 ?>
--- js/terminos.js
+++ js/terminos.js
@@ -1,5 +1,5 @@
 Swal.fire({
-	html: 'Por favor acepte nuestros <a target="_blank" href="https://drive.google.com/file/d/1YnXXKfg-HLppNLNbpYPEqbnJrYH_aMqh/view?usp=sharing">terminos y condiciones</a>',
+	html: 'Por favor para continuar acepte los <a target="_blank" href="https://drive.google.com/file/d/1YnXXKfg-HLppNLNbpYPEqbnJrYH_aMqh/view?usp=sharing">Terminos y Condiciones de nuestro servicio</a>',
     confirmButtonText: 'Acepto',
     icon: 'info',
     //padding: '1rem',
